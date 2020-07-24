cda7f45d8b93e15239a64c9d9d8ae741e432a878
Leida_Gandy <leida.gandy@gmail.com>
2020-07-24 21:55:20 +0100

    move starter code into separate components

---
+++ src/MainPage.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import Book from "./Book";
+
+class MainPage extends Component {
+  render() {
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Currently Reading</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  <li>
+                    <Book />
+                  </li>
+                </ol>
+              </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Want to Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  <li>
+                    <Book />
+                  </li>
+                </ol>
+              </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  <li>
+                    <Book />
+                  </li>
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="open-search">
+          <button onClick={() => this.setState({ showSearchPage: true })}>
+            Add a book
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
